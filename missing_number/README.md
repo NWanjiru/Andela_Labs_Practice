912be9b76083dd1b62350542a06ad167d340d929
Nyawira Wanjiru <leenjiru1@gmail.com>
2017-03-19 19:23:18 +0300

    Update find_missing.py and README files

---
+++ missing_number/README.md
@@ -0,0 +1,9 @@
+#Find_Missing
+
+1. Takes two arrays all containing positive integers.
+2. Checks whether any of the arrays contains an extra number
+3. Returns the extra number. 
+
+ > For example: 
+ 1. `[1,2,3]` and `[1,2,3,4] should return `4`
+ 2. `[4,66,7]` and `[66,77,7,4]` should return `77` 
---
+++ missing_number/find_missing.py
@@ -0,0 +1,15 @@
+def find_missing(list1,list2):
+  """" Checks whether a number exists another lists and returns the extra number"""
+  a = 0
+  if list1 == list2:
+    return 0
+  
+  for i in list1:
+    if i not in list2:
+      a = i
+  
+  for i in list2:
+    if i not in list1:
+      a = i
+      
+  return a
