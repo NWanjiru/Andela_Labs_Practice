afa2685ff5ebd512b479abdfcd78231e77f0a1bc
Nyawira Wanjiru <leenjiru1@gmail.com>
2017-03-21 19:01:41 +0300

    Modify word_occurance.py

--- word_count/word_occurance.py
+++ word_count/word_occurance.py
@@ -1,11 +1,16 @@
 def words(sentence):
 	"""
-        Counts the occurrences or characters in a word
-    """
+	Counts the occurrences or characters in a word
+	"""
+	my_words = sentence.split()
 	my_dict = {}
 
-	for w in sentence.split():
-		my_dict[w] = sentence.count(w)
+	for w in my_words:
+		if w in my_dict:
+			my_dict[w] += 1
+
+		else:
+			my_dict[w] = 1
 
 	return(my_dict)
 
