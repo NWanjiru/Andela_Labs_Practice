ba55f960aa437d03204b8a79429654493501ab56
Nyawira Wanjiru <leenjiru1@gmail.com>
2017-03-22 15:24:37 +0300

    Add comment to word_occurance.py

--- word_count/word_occurance.py
+++ word_count/word_occurance.py
@@ -6,8 +6,8 @@ def words(sentence):
 	my_dict = {}
 
 	for w in my_words:
-		if w.isnumeric():
-			w = int(w)
+		if w.isnumeric(): # checks if the element is a number and sets it as an integer
+			w = int(w)	  #  ie '1' will be 1
 			
 		if w in my_dict:
 			my_dict[w] += 1
